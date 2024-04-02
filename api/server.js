e879131a3bcea4d0403c9a468058edc0e4dd64d3
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-02 14:56:10 +0100

    Adds rewrite rule for 'pensamento' endpoint

--- api/server.js
+++ api/server.js
@@ -1,17 +1,21 @@
 // See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('database.json');
-const middlewares = jsonServer.defaults();
-const PORT = process.env.PORT || 3000;
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('database.json')
+const middlewares = jsonServer.defaults()
+const PORT = process.env.PORT || 3000
 
-server.use(middlewares);
+server.use(middlewares)
 
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/pensamento*': '/$1'
+}))
 
-server.use(router);
+server.use(router)
 
 server.listen(PORT, () => {
-    console.log(`JSON Server is running on port ${PORT} 🚀`);
+    console.log(`JSON Server is running on port ${PORT} 🚀`)
 })
 
 module.exports = server
