a3e335c3d72ee40fa9e0c2cd9cd3c47f8d974285
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-02 14:05:39 +0100

    New changes - AA

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('database.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('database.json');
+const middlewares = jsonServer.defaults();
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running 🚀')
-})
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log(`JSON Server is running on port ${PORT} 🚀`);
+});
