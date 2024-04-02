7fa9ef39523141a95e63c31e19fa8bb6ae4031b0
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-02 14:46:00 +0100

    New settings - AA

---
+++ api/server.js
@@ -0,0 +1,17 @@
+// See https://github.com/typicode/json-server#module
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('database.json');
+const middlewares = jsonServer.defaults();
+const PORT = process.env.PORT || 3000;
+
+server.use(middlewares);
+
+
+server.use(router);
+
+server.listen(PORT, () => {
+    console.log(`JSON Server is running on port ${PORT} 🚀`);
+})
+
+module.exports = server
