67cff7cccba6295b01b4762f2a36f3387b01d8dd
Aldair Avelino <aldair03avelino@gmail.com>
2024-04-03 11:26:07 +0100

    Refactor: Adapt routes to handle data in the expected format - AA

--- api/server.js
+++ api/server.js
@@ -1,23 +1,23 @@
 const express = require('express');
-const admin = require('firebase-admin')
-const path = require('path')
+const admin = require('firebase-admin');
+const path = require('path');
 require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
 
 const serviceAccountKey = {
-    "type": process.env.TYPE,
-    "project_id": process.env.PROJECT_ID,
-    "private_key_id": process.env.PRIVATE_KEY_ID,
-    "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "client_email": process.env.CLIENT_EMAIL,
-    "client_id": process.env.CLIENT_ID,
-    "auth_uri": process.env.AUTH_URI,
-    "token_uri": process.env.TOKEN_URI,
-    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
-    "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
-    "universe_domain": process.env.UNIVERSE_DOMAIN
-  };
+  "type": process.env.TYPE,
+  "project_id": process.env.PROJECT_ID,
+  "private_key_id": process.env.PRIVATE_KEY_ID,
+  "private_key": process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+  "client_email": process.env.CLIENT_EMAIL,
+  "client_id": process.env.CLIENT_ID,
+  "auth_uri": process.env.AUTH_URI,
+  "token_uri": process.env.TOKEN_URI,
+  "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER_X509_CERT_URL,
+  "client_x509_cert_url": process.env.CLIENT_X509_CERT_URL,
+  "universe_domain": process.env.UNIVERSE_DOMAIN
+};
 
-  const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccountKey),
@@ -27,63 +27,85 @@ admin.initializeApp({
 const app = express();
 
 app.use(express.json()); // Para usar req.body em POST
-
-// Rota GET para exibir a página inicial
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './../public/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public'))); // Servir arquivos estáticos na pasta 'public'
 
 // Rota GET para obter todos os pensamentos
 app.get('/pensamentos', async (req, res) => {
-  const db = admin.database();
-  const pensamentosRef = db.ref('pensamentos');
-  const snapshot = await pensamentosRef.once('value');
-  const pensamentos = snapshot.val();
-  res.send(pensamentos);
+  try {
+    const db = admin.database();
+    const pensamentosRef = db.ref('pensamentos');
+    const snapshot = await pensamentosRef.once('value');
+    const pensamentos = snapshot.val();
+    const pensamentoArray = Object.keys(pensamentos).map(key => ({ id: key, ...pensamentos[key] }));
+    res.json({ pensamento: pensamentoArray });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Rota POST para criar um novo pensamento
 app.post('/pensamentos', async (req, res) => {
-  const db = admin.database();
-  const pensamentosRef = db.ref('pensamentos');
-  const novoPensamento = req.body;
-  const id = pensamentosRef.push().key;
-  novoPensamento.id = id;
-  await pensamentosRef.child(id).set(novoPensamento);
-  res.send(novoPensamento);
+  try {
+    const db = admin.database();
+    const pensamentosRef = db.ref('pensamentos');
+    const novoPensamento = req.body;
+    const id = pensamentosRef.push().key;
+    novoPensamento.id = id;
+    await pensamentosRef.child(id).set(novoPensamento);
+    res.json({ pensamento: novoPensamento });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Rota GET para obter um único pensamento por ID
 app.get('/pensamentos/:id', async (req, res) => {
-  const db = admin.database();
-  const pensamentosRef = db.ref('pensamentos');
-  const id = req.params.id;
-  const snapshot = await pensamentosRef.child(id).once('value');
-  const pensamento = snapshot.val();
-  if (!pensamento) {
-    res.status(404).send('Pensamento não encontrado');
-    return;
+  try {
+    const db = admin.database();
+    const pensamentosRef = db.ref('pensamentos');
+    const id = req.params.id;
+    const snapshot = await pensamentosRef.child(id).once('value');
+    const pensamento = snapshot.val();
+    if (!pensamento) {
+      res.status(404).json({ message: 'Pensamento não encontrado' });
+      return;
+    }
+    res.json({ pensamento });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  res.send(pensamento);
 });
 
 // Rota PUT para atualizar um pensamento por ID
 app.put('/pensamentos/:id', async (req, res) => {
-  const db = admin.database();
-  const pensamentosRef = db.ref('pensamentos');
-  const id = req.params.id;
-  const pensamentoAtualizado = req.body;
-  await pensamentosRef.child(id).update(pensamentoAtualizado);
-  res.send(pensamentoAtualizado);
+  try {
+    const db = admin.database();
+    const pensamentosRef = db.ref('pensamentos');
+    const id = req.params.id;
+    const pensamentoAtualizado = req.body;
+    await pensamentosRef.child(id).update(pensamentoAtualizado);
+    res.json({ pensamento: pensamentoAtualizado });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Rota DELETE para remover um pensamento por ID
 app.delete('/pensamentos/:id', async (req, res) => {
-  const db = admin.database();
-  const pensamentosRef = db.ref('pensamentos');
-  const id = req.params.id;
-  await pensamentosRef.child(id).remove();
-  res.send('Pensamento removido com sucesso');
+  try {
+    const db = admin.database();
+    const pensamentosRef = db.ref('pensamentos');
+    const id = req.params.id;
+    await pensamentosRef.child(id).remove();
+    res.json({ message: 'Pensamento removido com sucesso' });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Rota GET para exibir a página inicial
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
